5539f5d52ba74b51c7398daa6299f84b60007ff3
Hans Zeller <hzeller@vmware.com>
2020-05-13 22:07:25 -0700

    Make DbgPrint and OsPrint methods on CRefCount
    Create a single DbgPrint() method on the CRefCount class. Also create
    a virtual OsPrint() method, making some objects derived from CRefCount
    easier to print from the debugger.
    
    Note that not all the OsPrint methods had the same signatures, some
    additional OsPrintxxx() methods have been generated for that.

--- src/backend/gporca/libgpopt/src/search/CGroup.cpp
+++ src/backend/gporca/libgpopt/src/search/CGroup.cpp
@@ -1717,14 +1717,14 @@ CGroup::OsPrintGrpOptCtxts
 	(
 	IOstream &os,
 	const CHAR *szPrefix
-	)
+	) const
 {
 	if (!FScalar() && !FDuplicateGroup() && GPOS_FTRACE(EopttracePrintOptimizationContext))
 	{
 		os << szPrefix << "Grp OptCtxts:" << std::endl;
 
 		COptimizationContext *poc = NULL;
-		ShtIter shtit(m_sht);
+		ShtIter shtit(const_cast<CGroup *>(this)->m_sht);
 		while (shtit.Advance())
 		{
 			{
@@ -1735,7 +1735,7 @@ CGroup::OsPrintGrpOptCtxts
 			if (NULL != poc)
 			{
 				os << szPrefix;
-				(void) poc->OsPrint(os, szPrefix);
+				(void) poc->OsPrintWithPrefix(os, szPrefix);
 			}
 
 			GPOS_CHECK_ABORT;
@@ -1759,7 +1759,7 @@ CGroup::OsPrintGrpScalarProps
 	(
 	IOstream &os,
 	const CHAR *szPrefix
-	)
+	) const
 {
 	GPOS_ASSERT(FScalar());
 
@@ -1827,7 +1827,7 @@ CGroup::OsPrintGrpProps
 	(
 	IOstream &os,
 	const CHAR *szPrefix
-	)
+	) const
 {
 	if (!FDuplicateGroup() && GPOS_FTRACE(EopttracePrintGroupProperties))
 	{
@@ -2092,7 +2092,7 @@ IOstream &
 CGroup::OsPrint
 	(
 	IOstream &os
-	)
+	) const
 {
 	const CHAR *szPrefix = "  ";
 	os << std::endl << "Group " << m_id << " (";
@@ -2114,7 +2114,7 @@ CGroup::OsPrint
 	CGroupExpression *pgexpr = m_listGExprs.First();
 	while (NULL != pgexpr)
 	{
-		(void) pgexpr->OsPrint(os, szPrefix);
+		(void) pgexpr->OsPrintWithPrefix(os, szPrefix);
 		pgexpr = m_listGExprs.Next(pgexpr);
 
 		GPOS_CHECK_ABORT;
@@ -2208,7 +2208,7 @@ CGroup::CostLowerBound
 //
 //---------------------------------------------------------------------------
 void
-CGroup::DbgPrint()
+CGroup::DbgPrintWithProperties()
 {
 	CAutoTraceFlag atf(EopttracePrintGroupProperties, true);
 	CAutoTrace at(m_mp);
